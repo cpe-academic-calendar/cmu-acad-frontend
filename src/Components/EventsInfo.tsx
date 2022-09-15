e2b4c814ff5be3ea8730b2e185b75e6271fd8ae5
TangJetsada123 <Jetsada_Jinaka@hotmail.com>
2022-09-15 13:52:50 +0700

    feat: create event info conponents

---
+++ src/Components/EventsInfo.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  BoxColor,
+  ButtonContainer,
+  Description,
+  DescriptionHead,
+  Duration,
+  InfoContainer,
+  InfoLayout,
+  Status,
+  StatusType,
+  Title,
+  TitleHeader,
+} from "./EventInfo.styled";
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+
+const EventInfo: React.FC = () => {
+  return (
+    <div>
+      <InfoContainer>
+        <InfoLayout>
+          <TitleHeader>
+            <BoxColor></BoxColor>
+            <Title>สอบกลางภาค</Title>
+          </TitleHeader>
+          <Duration>จ.-ส. 25 กรกฏาคม - 30 กรฏาคม</Duration>
+          <Status>
+            สถานะ:
+            <StatusType>วันสอบ</StatusType>
+          </Status>
+          <DescriptionHead>
+            คำอธิบาย:
+            <Description>วันสอบกลางภาค</Description>
+          </DescriptionHead>
+        </InfoLayout>
+        <ButtonContainer>
+          <button>
+            <EditOutlinedIcon />
+          </button>
+          <button>
+            <DeleteOutlineOutlinedIcon />
+          </button>
+          <button>
+            <CloseOutlinedIcon />
+          </button>
+        </ButtonContainer>
+      </InfoContainer>
+    </div>
+  );
+};
+
+export default EventInfo;
