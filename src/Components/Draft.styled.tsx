7618707b8ecceba25e64e21bf3439e3ce3aacda6
TangJetsada123 <Jetsada_Jinaka@hotmail.com>
2022-09-16 18:04:41 +0700

    feat: get month to draft

--- src/Components/Draft.styled.tsx
+++ src/Components/Draft.styled.tsx
@@ -2,6 +2,5 @@ import styled from 'styled-components'
 
 export const DraftContainer = styled.div`
     margin-top: 200px;
-    display: flex;
-    flex-wrap: wrap;
+    
 `
--- src/Components/Draft.tsx
+++ src/Components/Draft.tsx
@@ -1,28 +1,63 @@
 import React, { useState } from "react";
 import { Calendar } from "react-calendar";
 import { DraftContainer } from "./Draft.styled";
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  getDay,
+  isEqual,
+  isSameDay,
+  isSameMonth,
+  isToday,
+  parse,
+  parseISO,
+  startOfToday,
+} from "date-fns";
 
 export default function Draft() {
-  const [value, onChange] = useState(new Date());
+  let years = new Date().getFullYear();
+  let today = new Date(years, 0, 1);
+  let [selectDay,setSelectDay] = useState(today)
+  let [current,setCurrent] = useState(format(new Date(years, 0, 1), "MMM-yyyy"))
+  let firstMonth = parse(current, "MMM-yyyy", new Date());
+  let days = eachDayOfInterval({
+    start: firstMonth,
+    end: endOfMonth(firstMonth),
+  });
+
+  const day = days.map((data,dayIdx)=> {
+    
+  })
+
+  function classNames(...classes: any[]) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   return (
-    <div>
-      <div className='absolute bottom-0 left-0'>
-        <DraftContainer>
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-          <Calendar onChange={onChange} value={value} />
-        </DraftContainer>
+    <DraftContainer>
+      <div className="grid grid-cols-7 mt-2 text-sm">
+        {/* {days.map((day, dayIdx) => {
+          <div
+            key={day.toString()}
+            className={classNames(
+              dayIdx === 0 && colStartClasses[getDay(day)],
+              "py-15"
+            )}
+          ></div>;
+        })} */}
       </div>
-    </div>
+    </DraftContainer>
   );
 }
+
+let colStartClasses = [
+  "",
+  "col-start-2",
+  "col-start-3",
+  "col-start-4",
+  "col-start-5",
+  "col-start-6",
+  "col-start-7",
+];
