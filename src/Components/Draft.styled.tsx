666343d0d1e2eebd746f9451cf5463448896829e
TangJetsada123 <Jetsada_Jinaka@hotmail.com>
2022-09-20 17:45:53 +0700

    fix: check logic render calendar

--- src/Components/Draft.styled.tsx
+++ src/Components/Draft.styled.tsx
@@ -1,6 +1,27 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const DraftContainer = styled.div`
-    margin-top: 200px;
-    
-`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+margin: 149px 50px 14px 250px;
+grid-gap: 100px 10px;
+`;
+
+export const CalendarContainer = styled.div`
+  .date-button {
+    background-color: white;
+    border: none;
+  }
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 5px;
+  width: 25px;
+`;
+
+export const Calendar = styled.div``;
+
+export const CalendarMonth = styled.div``;
+
+export const LabelHeader = styled.div`
+  display: flex;
+`;
--- src/Components/Draft.tsx
+++ src/Components/Draft.tsx
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
-import { Calendar } from "react-calendar";
-import { DraftContainer } from "./Draft.styled";
+import React, { useCallback, useState } from "react";
+import {
+  CalendarContainer,
+  CalendarMonth,
+  DraftContainer,
+  LabelHeader,
+} from "./Draft.styled";
 import {
   add,
   eachDayOfInterval,
@@ -8,47 +12,39 @@ import {
   format,
   getDay,
   isEqual,
-  isSameDay,
   isSameMonth,
   isToday,
   parse,
-  parseISO,
-  startOfToday,
 } from "date-fns";
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Draft() {
   let years = new Date().getFullYear();
-  let today = new Date(years, 0, 1);
-  let [selectDay,setSelectDay] = useState(today)
-  let [current,setCurrent] = useState(format(new Date(years, 0, 1), "MMM-yyyy"))
+  let today = new Date(years);
+  let [selectedDay, setSelectDay] = useState(today);
+
+  let [current, setCurrent] = useState(
+    format(new Date(years, 0, 1), "MMM-yyyy")
+  );
+  let [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
   let firstMonth = parse(current, "MMM-yyyy", new Date());
   let days = eachDayOfInterval({
     start: firstMonth,
     end: endOfMonth(firstMonth),
   });
-
-  const day = days.map((data,dayIdx)=> {
-    
-  })
-
-  function classNames(...classes: any[]) {
-    return classes.filter(Boolean).join(" ");
-  }
+  const label = ["อา", "จ", "อ", "พ", "พฤ", "ศ", "ส"];
+  const dayLabel = label.map((day) => day);
+  console.log(days.map((data)=> data.getDay()))
 
   return (
-    <DraftContainer>
-      <div className="grid grid-cols-7 mt-2 text-sm">
-        {/* {days.map((day, dayIdx) => {
-          <div
-            key={day.toString()}
-            className={classNames(
-              dayIdx === 0 && colStartClasses[getDay(day)],
-              "py-15"
-            )}
-          ></div>;
-        })} */}
-      </div>
-    </DraftContainer>
+    <div>
+
+       
+    </div>
+              
   );
 }
 
